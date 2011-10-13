KyryloNikolaiev/Django-djangoproject

commit b6412e4a2f7001b6b4423f1e7770b47432ca96bc

Fixed #12467 -- Made the model data validation for `DateField` and `DateTimeField` more useful by actually telling what was the value that failed. Also did a bit of PEP8 cleanup in the area. Thanks to knutin for the report, to raulcd for the initial patch and to charettes for the review.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@16966 bcc190cf-cafb-0310-a4f2-bffc1f526a37

tests/modeltests/validation/test_error_messages.py
@@ -55,3 +55,59 @@ class ValidationMessagesTest(TestCase):
         except ValidationError, e:
             self.assertEqual(e.messages,
                         [u"'foo' value must be either None, True or False."])
+
+    def test_date_field_raises_error_message(self):
+        f = models.DateField()
+        self.assertRaises(ValidationError, f.clean, 'foo', None)
+        try:
+            f.clean('foo', None)
+        except ValidationError, e:
+            self.assertEqual(e.messages, [
+                u"'foo' value has an invalid date format. "
+                u"It must be in YYYY-MM-DD format."])
+
+        self.assertRaises(ValidationError, f.clean, 'aaaa-10-10', None)
+        try:
+            f.clean('aaaa-10-10', None)
+        except ValidationError, e:
+            self.assertEqual(e.messages, [
+                u"'aaaa-10-10' value has an invalid date format. "
+                u"It must be in YYYY-MM-DD format."])
+
+        self.assertRaises(ValidationError, f.clean, '2011-13-10', None)
+        try:
+            f.clean('2011-13-10', None)
+        except ValidationError, e:
+            self.assertEqual(e.messages, [
+                u"'2011-13-10' value has the correct format (YYYY-MM-DD) "
+                u"but it is an invalid date."])
+
+        self.assertRaises(ValidationError, f.clean, '2011-10-32', None)
+        try:
+            f.clean('2011-10-32', None)
+        except ValidationError, e:
+            self.assertEqual(e.messages, [
+                u"'2011-10-32' value has the correct format (YYYY-MM-DD) "
+                u"but it is an invalid date."])
+
+    def test_datetime_field_raises_error_message(self):
+        f = models.DateTimeField()
+        # Wrong format
+        self.assertRaises(ValidationError, f.clean, 'foo', None)
+        try:
+            f.clean('foo', None)
+        except ValidationError, e:
+            self.assertEqual(e.messages, [
+                u"'foo' value either has an invalid valid format "
+                u"(The format must be YYYY-MM-DD HH:MM[:ss[.uuuuuu]]) "
+                u"or is an invalid date/time."])
+        self.assertRaises(ValidationError, f.clean,
+                          '2011-10-32 10:10', None)
+        # Correct format but invalid date/time
+        try:
+            f.clean('2011-10-32 10:10', None)
+        except ValidationError, e:
+            self.assertEqual(e.messages, [
+                u"'2011-10-32 10:10' value either has an invalid valid format "
+                u"(The format must be YYYY-MM-DD HH:MM[:ss[.uuuuuu]]) "
+                u"or is an invalid date/time."])

tests/modeltests/validation/tests.py
@@ -12,7 +12,7 @@ from modeltests.validation.validators import TestModelsWithValidators
 from modeltests.validation.test_unique import (GetUniqueCheckTests,
     PerformUniqueChecksTest)
 from modeltests.validation.test_custom_messages import CustomMessagesTest
-
+from modeltests.validation.test_error_messages import ValidationMessagesTest
 
 class BaseModelValidationTests(ValidationTestCase):
 

commit 2593233aed218e5bf0063d6f64c26a4c8e55167b

Fixed a couple of `assert` syntax warnings mistakingly introduced in r16966 and added some tests to prevent future regressions. Thanks to Anssi Kääriäinen for the report. Refs #12467.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@16971 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/db/models/fields/__init__.py
@@ -510,8 +510,8 @@ class AutoField(Field):
         'invalid': _(u"'%s' value must be an integer."),
     }
     def __init__(self, *args, **kwargs):
-        assert (kwargs.get('primary_key', False) is True,
-                "%ss must have primary_key=True." % self.__class__.__name__)
+        assert kwargs.get('primary_key', False) is True, \
+               "%ss must have primary_key=True." % self.__class__.__name__
         kwargs['blank'] = True
         Field.__init__(self, *args, **kwargs)
 
@@ -536,8 +536,8 @@ class AutoField(Field):
         return int(value)
 
     def contribute_to_class(self, cls, name):
-        assert (not cls._meta.has_auto_field,
-                "A model can't have more than one AutoField.")
+        assert not cls._meta.has_auto_field, \
+               "A model can't have more than one AutoField."
         super(AutoField, self).contribute_to_class(cls, name)
         cls._meta.has_auto_field = True
         cls._meta.auto_field = self

tests/modeltests/validation/models.py
@@ -90,3 +90,13 @@ class GenericIPAddressTestModel(models.Model):
 
 class GenericIPAddrUnpackUniqueTest(models.Model):
     generic_v4unpack_ip = models.GenericIPAddressField(blank=True, unique=True, unpack_ipv4=True)
+
+
+try:
+    # A model can't have multiple AutoFields
+    # Refs #12467.
+    class MultipleAutoFields(models.Model):
+        auto1 = models.AutoField(primary_key=True)
+        auto2 = models.AutoField(primary_key=True)
+except AssertionError, e:
+    assert e.message == u"A model can't have more than one AutoField."

tests/modeltests/validation/test_error_messages.py
@@ -12,6 +12,12 @@ class ValidationMessagesTest(TestCase):
             f.clean('foo', None)
         except ValidationError, e:
             self.assertEqual(e.messages, [u"'foo' value must be an integer."])
+        # primary_key must be True. Refs #12467.
+        self.assertRaises(AssertionError, models.AutoField, 'primary_key', False)
+        try:
+            models.AutoField(primary_key=False)
+        except AssertionError, e:
+            self.assertEqual(e.message, u"AutoFields must have primary_key=True.")
 
     def test_integer_field_raises_error_message(self):
         f = models.IntegerField()
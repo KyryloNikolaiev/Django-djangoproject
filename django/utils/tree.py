django/django

commit fc6528b25ab1834be1a478b405bf8f7ec5cf860c

Fixed #28629 -- Made tree.Node instances hashable.
Regression in 508b5debfb16843a8443ebac82c1fb91f15da687 which
added Node.__eq__().

django/utils/tree.py
@@ -70,6 +70,9 @@ class Node:
             self.children == other.children
         )
 
+    def __hash__(self):
+        return hash((self.__class__, self.connector, self.negated) + tuple(self.children))
+
     def add(self, data, conn_type, squash=True):
         """
         Combine this tree and the data represented by data using the

tests/utils_tests/test_tree.py
@@ -19,6 +19,16 @@ class NodeTests(unittest.TestCase):
                          "<Node: (DEFAULT: ('a', 1), ('b', 2))>")
         self.assertEqual(repr(self.node2), "<Node: (DEFAULT: )>")
 
+    def test_hash(self):
+        node3 = Node(self.node1_children, negated=True)
+        node4 = Node(self.node1_children, connector='OTHER')
+        node5 = Node(self.node1_children)
+        self.assertNotEqual(hash(self.node1), hash(self.node2))
+        self.assertNotEqual(hash(self.node1), hash(node3))
+        self.assertNotEqual(hash(self.node1), hash(node4))
+        self.assertEqual(hash(self.node1), hash(node5))
+        self.assertEqual(hash(self.node2), hash(Node()))
+
     def test_len(self):
         self.assertEqual(len(self.node1), 2)
         self.assertEqual(len(self.node2), 0)

KyryloNikolaiev/Django-djangoproject

commit 016c539de1d8cd035ab55ac41bb0fc73ee7d41c4

Fixed #29838 -- Fixed crash when combining Q objects with __in lookups and lists.
Regression in fc6528b25ab1834be1a478b405bf8f7ec5cf860c.

django/utils/tree.py
@@ -5,6 +5,8 @@ ORM.
 
 import copy
 
+from django.utils.hashable import make_hashable
+
 
 class Node:
     """
@@ -71,10 +73,7 @@ class Node:
         )
 
     def __hash__(self):
-        return hash((self.__class__, self.connector, self.negated, *[
-            tuple(child) if isinstance(child, list) else child
-            for child in self.children
-        ]))
+        return hash((self.__class__, self.connector, self.negated, *make_hashable(self.children)))
 
     def add(self, data, conn_type, squash=True):
         """

tests/utils_tests/test_tree.py
@@ -24,12 +24,15 @@ class NodeTests(unittest.TestCase):
         node4 = Node(self.node1_children, connector='OTHER')
         node5 = Node(self.node1_children)
         node6 = Node([['a', 1], ['b', 2]])
+        node7 = Node([('a', [1, 2])])
+        node8 = Node([('a', (1, 2))])
         self.assertNotEqual(hash(self.node1), hash(self.node2))
         self.assertNotEqual(hash(self.node1), hash(node3))
         self.assertNotEqual(hash(self.node1), hash(node4))
         self.assertEqual(hash(self.node1), hash(node5))
         self.assertEqual(hash(self.node1), hash(node6))
         self.assertEqual(hash(self.node2), hash(Node()))
+        self.assertEqual(hash(node7), hash(node8))
 
     def test_len(self):
         self.assertEqual(len(self.node1), 2)
django/django

commit 88fc9e2826044110b7b22577a227f122fe9c1fb5

Fixed #25772 -- Corrected __len lookup on ArrayField for empty arrays.

django/contrib/postgres/fields/array.py
@@ -202,7 +202,11 @@ class ArrayLenTransform(Transform):
 
     def as_sql(self, compiler, connection):
         lhs, params = compiler.compile(self.lhs)
-        return 'array_length(%s, 1)' % lhs, params
+        # Distinguish NULL and empty arrays
+        return (
+            'CASE WHEN %(lhs)s IS NULL THEN NULL ELSE '
+            'coalesce(array_length(%(lhs)s, 1), 0) END'
+        ) % {'lhs': lhs}, params
 
 
 class IndexTransform(Transform):

tests/postgres_tests/test_array.py
@@ -231,6 +231,13 @@ class TestQuerying(PostgreSQLTestCase):
             self.objs[0:3]
         )
 
+    def test_len_empty_array(self):
+        obj = NullableIntegerArrayModel.objects.create(field=[])
+        self.assertSequenceEqual(
+            NullableIntegerArrayModel.objects.filter(field__len=0),
+            [obj]
+        )
+
     def test_slice(self):
         self.assertSequenceEqual(
             NullableIntegerArrayModel.objects.filter(field__0_1=[2]),

KyryloNikolaiev/Django-djangoproject

commit 4b37129c93f718678ab5df3f40a7a3a860619d54

Fixed #34205 -- Fixed Meta.constraints validation crash with ArrayField and __len lookup.
Regression in 88fc9e2826044110b7b22577a227f122fe9c1fb5 that began
manifesting in Django 4.1.

AUTHORS
@@ -431,6 +431,7 @@ answer newbie questions, and generally made Django that much better:
     james_027@yahoo.com
     James Aylett
     James Bennett <james@b-list.org>
+    James Gillard <jamesgillard@live.co.uk>
     James Murty
     James Tauber <jtauber@jtauber.com>
     James Timmins <jameshtimmins@gmail.com>

django/contrib/postgres/fields/array.py
@@ -297,7 +297,7 @@ class ArrayLenTransform(Transform):
         return (
             "CASE WHEN %(lhs)s IS NULL THEN NULL ELSE "
             "coalesce(array_length(%(lhs)s, 1), 0) END"
-        ) % {"lhs": lhs}, params
+        ) % {"lhs": lhs}, params * 2
 
 
 @ArrayField.register_lookup

tests/postgres_tests/test_constraints.py
@@ -76,6 +76,16 @@ class SchemaTests(PostgreSQLTestCase):
             constraint.validate(IntegerArrayModel, IntegerArrayModel())
         constraint.validate(IntegerArrayModel, IntegerArrayModel(field=[1]))
 
+    def test_check_constraint_array_length(self):
+        constraint = CheckConstraint(
+            check=Q(field__len=1),
+            name="array_length",
+        )
+        msg = f"Constraint “{constraint.name}” is violated."
+        with self.assertRaisesMessage(ValidationError, msg):
+            constraint.validate(IntegerArrayModel, IntegerArrayModel())
+        constraint.validate(IntegerArrayModel, IntegerArrayModel(field=[1]))
+
     def test_check_constraint_daterange_contains(self):
         constraint_name = "dates_contains"
         self.assertNotIn(
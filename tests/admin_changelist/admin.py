django/django

commit 27aa7035f57f0db30b6632e4274e18b430906799

Fixed #27471 -- Made admin's filter choices collapsable.

django/contrib/admin/static/admin/js/filters.js
@@ -1,0 +1,30 @@
+/**
+ * Persist changelist filters state (collapsed/expanded).
+ */
+'use strict';
+{
+    // Init filters.
+    let filters = JSON.parse(sessionStorage.getItem('django.admin.filtersState'));
+
+    if (!filters) {
+        filters = {};
+    }
+
+    Object.entries(filters).forEach(([key, value]) => {
+        const detailElement = document.querySelector(`[data-filter-title='${key}']`);
+
+        // Check if the filter is present, it could be from other view.
+        if (detailElement) {
+            value ? detailElement.setAttribute('open', '') : detailElement.removeAttribute('open');
+        }
+    });
+
+    // Save filter state when clicks.
+    const details = document.querySelectorAll('details');
+    details.forEach(detail => {
+        detail.addEventListener('toggle', event => {
+            filters[`${event.target.dataset.filterTitle}`] = detail.open;
+            sessionStorage.setItem('django.admin.filtersState', JSON.stringify(filters));
+        });
+    });
+}

tests/admin_changelist/tests.py
@@ -1810,3 +1810,55 @@ class SeleniumTests(AdminSeleniumTestCase):
             )
         finally:
             alert.dismiss()
+
+    def test_collapse_filters(self):
+        from selenium.webdriver.common.by import By
+
+        self.admin_login(username="super", password="secret")
+        self.selenium.get(self.live_server_url + reverse("admin:auth_user_changelist"))
+
+        # The UserAdmin has 3 field filters by default: "staff status",
+        # "superuser status", and "active".
+        details = self.selenium.find_elements(By.CSS_SELECTOR, "details")
+        # All filters are opened at first.
+        for detail in details:
+            self.assertTrue(detail.get_attribute("open"))
+        # Collapse "staff' and "superuser" filters.
+        for detail in details[:2]:
+            summary = detail.find_element(By.CSS_SELECTOR, "summary")
+            summary.click()
+            self.assertFalse(detail.get_attribute("open"))
+        # Filters are in the same state after refresh.
+        self.selenium.refresh()
+        self.assertFalse(
+            self.selenium.find_element(
+                By.CSS_SELECTOR, "[data-filter-title='staff status']"
+            ).get_attribute("open")
+        )
+        self.assertFalse(
+            self.selenium.find_element(
+                By.CSS_SELECTOR, "[data-filter-title='superuser status']"
+            ).get_attribute("open")
+        )
+        self.assertTrue(
+            self.selenium.find_element(
+                By.CSS_SELECTOR, "[data-filter-title='active']"
+            ).get_attribute("open")
+        )
+        # Collapse a filter on another view (Bands).
+        self.selenium.get(
+            self.live_server_url + reverse("admin:admin_changelist_band_changelist")
+        )
+        self.selenium.find_element(By.CSS_SELECTOR, "summary").click()
+        # Go to Users view and then, back again to Bands view.
+        self.selenium.get(self.live_server_url + reverse("admin:auth_user_changelist"))
+        self.selenium.get(
+            self.live_server_url + reverse("admin:admin_changelist_band_changelist")
+        )
+        # The filter remains in the same state.
+        self.assertFalse(
+            self.selenium.find_element(
+                By.CSS_SELECTOR,
+                "[data-filter-title='number of members']",
+            ).get_attribute("open")
+        )

django/contrib/admin/static/admin/css/changelists.css
@@ -148,10 +148,33 @@
     border-bottom: none;
 }
 
-#changelist-filter h3 {
+#changelist-filter h3,
+#changelist-filter details summary {
     font-weight: 400;
     padding: 0 15px;
     margin-bottom: 10px;
+}
+
+#changelist-filter details summary > * {
+    display: inline;
+}
+
+#changelist-filter details > summary {
+    list-style-type: none;
+}
+
+#changelist-filter details > summary::-webkit-details-marker {
+    display: none;
+}
+
+#changelist-filter details > summary::before {
+    content: '→';
+    font-weight: bold;
+    color: var(--link-hover-color);
+}
+
+#changelist-filter details[open] > summary::before {
+    content: '↓';
 }
 
 #changelist-filter ul {

django/contrib/admin/templates/admin/change_list.html
@@ -21,6 +21,7 @@
 {% block extrahead %}
 {{ block.super }}
 {{ media.js }}
+<script src="{% static 'admin/js/filters.js' %}" defer></script>
 {% endblock %}
 
 {% block bodyclass %}{{ block.super }} app-{{ opts.app_label }} model-{{ opts.model_name }} change-list{% endblock %}

django/contrib/admin/templates/admin/filter.html
@@ -1,8 +1,12 @@
 {% load i18n %}
-<h3>{% blocktranslate with filter_title=title %} By {{ filter_title }} {% endblocktranslate %}</h3>
-<ul>
-{% for choice in choices %}
+<details data-filter-title="{{ title }}" open>
+  <summary>
+    {% blocktranslate with filter_title=title %} By {{ filter_title }} {% endblocktranslate %}
+  </summary>
+  <ul>
+  {% for choice in choices %}
     <li{% if choice.selected %} class="selected"{% endif %}>
     <a href="{{ choice.query_string|iriencode }}">{{ choice.display }}</a></li>
-{% endfor %}
-</ul>
+  {% endfor %}
+  </ul>
+</details>

KyryloNikolaiev/Django-djangoproject

commit 1bc93c0a8fc9293f50564edb7622b2c7e29fc6f4

Fixed #34283 -- Escaped title in admin's changelist filters.
Regression in 27aa7035f57f0db30b6632e4274e18b430906799.

django/contrib/admin/static/admin/js/filters.js
@@ -11,7 +11,7 @@
     }
 
     Object.entries(filters).forEach(([key, value]) => {
-        const detailElement = document.querySelector(`[data-filter-title='${key}']`);
+        const detailElement = document.querySelector(`[data-filter-title='${CSS.escape(key)}']`);
 
         // Check if the filter is present, it could be from other view.
         if (detailElement) {

tests/admin_changelist/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 
-from .models import Band, Child, Event, Parent, Swallow
+from .models import Band, Child, Event, Parent, ProxyUser, Swallow
 
 site = admin.AdminSite(name="admin")
 
@@ -185,3 +185,24 @@ class EmptyValueChildAdmin(admin.ModelAdmin):
     @admin.display(empty_value="&dagger;")
     def age_display(self, obj):
         return obj.age
+
+
+class UnescapedTitleFilter(admin.SimpleListFilter):
+    title = "It's OK"
+    parameter_name = "is_active"
+
+    def lookups(self, request, model_admin):
+        return [("yes", "yes"), ("no", "no")]
+
+    def queryset(self, request, queryset):
+        if self.value() == "yes":
+            return queryset.filter(is_active=True)
+        else:
+            return queryset.filter(is_active=False)
+
+
+class CustomUserAdmin(UserAdmin):
+    list_filter = [UnescapedTitleFilter]
+
+
+site.register(ProxyUser, CustomUserAdmin)

tests/admin_changelist/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -121,3 +122,8 @@ class CustomIdUser(models.Model):
 
 class CharPK(models.Model):
     char_pk = models.CharField(max_length=100, primary_key=True)
+
+
+class ProxyUser(User):
+    class Meta:
+        proxy = True

tests/admin_changelist/tests.py
@@ -1910,3 +1910,23 @@ class SeleniumTests(AdminSeleniumTestCase):
                 "[data-filter-title='number of members']",
             ).get_attribute("open")
         )
+
+    def test_collapse_filter_with_unescaped_title(self):
+        from selenium.webdriver.common.by import By
+
+        self.admin_login(username="super", password="secret")
+        changelist_url = reverse("admin:admin_changelist_proxyuser_changelist")
+        self.selenium.get(self.live_server_url + changelist_url)
+        # Title is escaped.
+        filter_title = self.selenium.find_element(
+            By.CSS_SELECTOR, "[data-filter-title='It\\'s OK']"
+        )
+        filter_title.find_element(By.CSS_SELECTOR, "summary").click()
+        self.assertFalse(filter_title.get_attribute("open"))
+        # Filter is in the same state after refresh.
+        self.selenium.refresh()
+        self.assertFalse(
+            self.selenium.find_element(
+                By.CSS_SELECTOR, "[data-filter-title='It\\'s OK']"
+            ).get_attribute("open")
+        )